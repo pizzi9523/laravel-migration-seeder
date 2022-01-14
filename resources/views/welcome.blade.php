538e99661ccf706c648b27aabfce0a9a5e0dc1b2
pizzi9523 <pizzitolagiuseppe@live.it>
2022-01-14 18:53:17 +0100

    create layout and add style to my welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,40 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+<h1 class="text-center mt-5">WELCOME IN MYTRAVELAGENCY</h1>
+<div class="container">
+    <div class="row my-5">
+        @foreach($packages as $package)
+        <div class="col text-center">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{$package->name}}</h5>
+                    <p class="card-text">{{$package->description}}</p>
+                </div>
+  
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Destinazione: <strong>{{$package->destination}}</strong></li>
+                    <li class="list-group-item">Partenza da: <strong>{{$package->departure_from}}</strong></p></li>
+                    <li class="list-group-item">Data di Partenza: <strong>{{$package->departure_date}}</strong> </li>
+                    <li class="list-group-item">Data di Ritorno: <strong>{{$package->return_date}}</strong> </li>
+                    <li class="list-group-item">Prezzo: <strong>{{$package->price}}€</strong></li>
 
-    <title>Travel Agency</title>
+                </ul>
+  
+</div>
+            
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+            
 
-    <!-- Styles -->
-
-</head>
-
-<body>
-    @foreach($packages as $package)
-    <div class="card">
-        <h1>{{$package->name}}</h1>
-        <p>Partenza da: <strong>{{$package->departure_from}}</strong></p>
-        <p>Destinazione: <strong>{{$package->destination}}</strong> </p>
-        <p>Data di Partenza: <strong>{{$package->departure_date}}</strong> </p>
-        <p>Data di Ritorno: <strong>{{$package->return_date}}</strong> </p>
-        <p>Data di Ritorno: <strong>{{$package->price}}€</strong> </p>
-    </div>
-
+        </div>
     @endforeach
-</body>
+    </div>
+</div>
 
-</html>
+
+
+
+    
+    
+@endsection
+
