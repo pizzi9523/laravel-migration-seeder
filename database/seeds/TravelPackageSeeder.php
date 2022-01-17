7043af87ef03ad89ba064cba321b4450ff75f74a
pizzi9523 <pizzitolagiuseppe@live.it>
2022-01-17 15:40:53 +0100

    popolate table with fake data
    Use the seeder to popolate the table travel_packages database

--- database/seeds/TravelPackageSeeder.php
+++ database/seeds/TravelPackageSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\TravelPackage;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TravelPackageSeeder extends Seeder
 {
@@ -9,8 +11,18 @@ class TravelPackageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 84; $i++) {
+            $_package = new TravelPackage();
+            $_package->name = $faker->name();
+            $_package->description = $faker->paragraph();
+            $_package->departure_from = $faker->city();
+            $_package->destination = $faker->city();
+            $_package->departure_date = $faker->date();
+            $_package->return_date = $faker->date();
+            $_package->price = $faker->randomFloat(2, 0, 5000);
+            $_package->save();
+        }
     }
 }
